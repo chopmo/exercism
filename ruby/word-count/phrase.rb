14e771eb826efe295ba8598fd3966658e178d88d
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2013-09-25 00:26:51 +0200

    Split out a word counter class, but still not happy with a lot of naming.

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,17 +1,35 @@
+class WordCounter
+  attr_reader :word_counts
+  def initialize
+    @word_counts = Hash.new(0)
+  end
+
+  def count(word)
+    word_counts[word.downcase] += 1
+  end
+end
+
 class Phrase
   attr_reader :text
+  attr_reader :word_counter
   def initialize(text)
     @text = text
+    @word_counter = WordCounter.new
   end
 
   def word_count
-    words = text.split(/[^\w]+/)
+    @word_count ||= build_word_count
+  end
 
-    word_counts = Hash.new(0)
+  def build_word_count
     words.each do |word|
-      word_counts[word.downcase] += 1
+      word_counter.count(word)
     end
 
-    word_counts
+    word_counter.word_counts
+  end
+
+  def words
+    text.split(/[^\w]+/)
   end
 end
