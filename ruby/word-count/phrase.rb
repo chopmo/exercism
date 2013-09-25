678ba3a8c03f1ad1991515aa7241642bf94e5711
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2013-09-25 09:37:26 +0200

    Even more decoupled design
    Split out phrase parser and word normalizer as separate classes.

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,3 +1,25 @@
+class PhraseParser
+  attr_reader :text
+  def initialize(text)
+    @text = text
+  end
+
+  def words
+    text.split(non_word_pattern)
+  end
+
+  private
+  def non_word_pattern
+    /\W+/
+  end
+end
+
+class DowncasingWordNormalizer
+  def normalize(word)
+    word.downcase
+  end
+end
+
 class WordCounter
   attr_reader :word_counts
   def initialize
@@ -5,31 +27,32 @@ class WordCounter
   end
 
   def count(word)
-    word_counts[word.downcase] += 1
+    word_counts[word] += 1
   end
 end
 
 class Phrase
-  attr_reader :text
+  attr_reader :phrase_parser
+  attr_reader :word_normalizer
   attr_reader :word_counter
+
   def initialize(text)
-    @text = text
+    @phrase_parser = PhraseParser.new(text)
+    @word_normalizer = DowncasingWordNormalizer.new
     @word_counter = WordCounter.new
   end
 
   def word_count
-    @word_count ||= build_word_count
+    @word_counts ||= build_word_counts
   end
 
-  def build_word_count
-    words.each do |word|
-      word_counter.count(word)
+  private
+  def build_word_counts
+    phrase_parser.words.each do |word|
+      normalized_word = word_normalizer.normalize(word)
+      word_counter.count(normalized_word)
     end
 
     word_counter.word_counts
-  end
-
-  def words
-    text.split(/[^\w]+/)
   end
 end
