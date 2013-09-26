d98c6bb8efa7fc2047871b02ed3b26bc35e4c643
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2013-09-26 09:26:54 +0200

    Went crazy on the word counts

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,3 +1,103 @@
+class Phrase
+  def initialize(text)
+    @text = text
+  end
+
+  def word_count
+    @word_counts ||= build_word_counts
+  end
+
+  private
+  attr_reader :text
+
+  def build_word_counts
+    counts = Hash.new { 0 }
+    normalized_words.each do |word|
+      counts[word] += 1
+    end
+    counts
+  end
+
+  def normalized_words
+    words.map { |word| word.downcase }
+  end
+
+  def words
+    text.scan(word_pattern)
+  end
+
+  def word_pattern
+    /\w+/
+  end
+end
+
+__END__
+### VERSION 1 ###
+class Phrase
+  attr_reader :word_count
+  def initialize(text)
+    @word_count = text.scan(/\w+/).map { |word| word.downcase }.each_with_object(Hash.new(0)) do |word, counts|
+      counts[word] += 1
+    end
+  end
+end
+
+### VERSION 2 ###
+class Phrase
+  def initialize(text)
+    @text = text
+  end
+
+  def word_count
+    @word_counts ||= build_word_counts
+  end
+
+  private
+  def build_word_counts
+    words = @text.scan(/\w+/)
+    normalized_words = words.map { |word| word.downcase }
+    normalized_words.each_with_object(Hash.new(0)) do |word, counts|
+      counts[word] += 1
+    end
+  end
+end
+
+
+### VERSION 3 ###
+class Phrase
+  def initialize(text)
+    @text = text
+  end
+
+  def word_count
+    @word_counts ||= build_word_counts
+  end
+
+  private
+  attr_reader :text
+
+  def build_word_counts
+    counts = Hash.new { 0 }
+    normalized_words.each do |word|
+      counts[word] += 1
+    end
+    counts
+  end
+
+  def normalized_words
+    words.map { |word| word.downcase }
+  end
+
+  def words
+    text.scan(word_pattern)
+  end
+
+  def word_pattern
+    /\w+/
+  end
+end
+
+### VERSION 4 ###
 class PhraseParser
   attr_reader :text
   def initialize(text)
