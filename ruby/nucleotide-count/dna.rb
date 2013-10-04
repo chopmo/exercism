7753d99dad8741035ae5682db2064d6c03ea0e5d
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2013-10-04 09:59:11 +0200

    Factored out Nucleotide. Likely first submission.

--- ruby/nucleotide-count/dna.rb
+++ ruby/nucleotide-count/dna.rb
@@ -1,36 +1,60 @@
 class DNA
-
-  def initialize(seq)
-    validate_dna(seq)
-    @seq = seq
+  def initialize(symbols)
+    @sequence = build_sequence(symbols)
+    raise ArgumentError unless valid_dna_sequence?
   end
 
-  def count(nucleotide)
-    unless nucleotides.include?(nucleotide)
-      raise ArgumentError
-    end
-
-    @seq.count(nucleotide)
+  def count(symbol)
+    nucleotide = make_nucleotide(symbol)
+    sequence.count(nucleotide)
   end
 
   def nucleotide_counts
-    dna_nucleotides.each_with_object({}) do |nucleotide, counts|
-      counts[nucleotide] = count(nucleotide)
-    end
+    counts = dna_symbols.map { |symbol| count(symbol) }
+    Hash[dna_symbols.zip(counts)]
   end
 
   private
-  def validate_dna(seq)
-    if seq.chars.any? { |nucleotide| !dna_nucleotides.include?(nucleotide) }
-      raise ArgumentError
-    end
+  attr_reader :sequence
+
+  def build_sequence(symbols)
+    symbols.chars.map { |symbol| make_nucleotide(symbol) }
   end
 
-  def nucleotides
-    %w(A T C G U)
+  def valid_dna_sequence?
+    sequence.all?(&:valid_in_dna?)
   end
 
-  def dna_nucleotides
-    %w(A T C G)
+  def make_nucleotide(symbol)
+    Nucleotide.new(symbol)
+  end
+
+  def dna_symbols
+    Nucleotide::DNA_SYMBOLS
+  end
+end
+
+class Nucleotide
+  def initialize(symbol)
+    @symbol = symbol
+    raise ArgumentError unless valid_symbol?
+  end
+
+  def valid_in_dna?
+    DNA_SYMBOLS.include?(symbol)
+  end
+
+  def ==(other)
+    symbol == other.symbol
+  end
+
+  SYMBOLS = %w(A T C G U)
+  DNA_SYMBOLS = SYMBOLS - %w(U)
+
+  protected
+  attr_reader :symbol
+
+  def valid_symbol?
+    SYMBOLS.include?(symbol)
   end
 end
