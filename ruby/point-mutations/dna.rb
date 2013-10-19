409b5cefa0d8adef09d9c6f05e80e9f8f6496d05
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2013-10-19 10:16:12 +0200

    First iteration on point mutation

--- ruby/point-mutations/dna.rb
+++ ruby/point-mutations/dna.rb
@@ -1,13 +1,18 @@
 class DNA
-  attr_reader :sequence
-
   def initialize(symbols)
-    @sequence = Sequence.new(symbols)
+    @symbols = symbols
   end
 
   def hamming_distance(symbols)
     other_sequence = Sequence.new(symbols)
     sequence.hamming_distance(other_sequence)
+  end
+
+  private
+  attr_reader :symbols
+
+  def sequence
+    Sequence.new(symbols)
   end
 end
 
@@ -19,17 +24,24 @@ class Sequence
   end
 
   def hamming_distance(other)
-    nucleotide_pairs(other).count { |n1, n2| n1 != n2 }
+    nucleotide_pairs(other).count(&mutation_pair)
   end
 
   protected
   def nucleotide_pairs(other)
-    pairs = nucleotides.zip(other.nucleotides)
-    pairs.reject { |pair| pair.any?(&:nil?) }
+    nucleotides.zip(other.nucleotides).reject(&incomplete_pair)
   end
 
   def nucleotides
     symbols.chars.map { |symbol| Nucleotide.new(symbol) }
+  end
+
+  def mutation_pair
+    ->(pair) { pair[0] != pair[1] }
+  end
+
+  def incomplete_pair
+    ->(pair) { pair.any?(&:nil?) }
   end
 end
 
